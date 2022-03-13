9257c4926383b523c6f31880fd66cda846408d31
Kirill <kirill_1131@mail.ru>
2022-03-13 14:29:23 +0300

    + added UserItem

--- src/panels/Top/Users/UserItem/index.js
+++ src/panels/Top/Users/UserItem/index.js
@@ -1,39 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { Button, Tooltip } from "antd";
+import { Button, Tooltip, Badge, Avatar } from "antd";
 import { Router, Redirect } from "@reach/router";
 import { observer } from "mobx-react-lite";
-import { useStores } from "../../../hooks/useStores";
-import { numberToString } from "../../../utils";
+import { useStores } from "../../../../hooks/useStores";
+import { numberToString } from "../../../../utils";
 
-const UserItem = observer(() => {
+import "./UserItem.scss";
+
+const UserItem = observer(({ dataItem, topNumber }) => {
   const { PanelStore } = useStores();
-
+  console.log("dataItem.balance", dataItem.balance);
   return (
-    <div className="users">
-      <div className="users-list">
-        {mockProject.map((item) => {
-          return (
-            <div className="users-list__item" key={item.id}>
-              <img className="img" src={item.img} alt="img" />
-              <div className="item-container">
-                <div className="info">
-                  {item.firstName} {item.firstName}
-                </div>
-                <div className="info description">
-                  {numberToString(item.balance)}$
-                </div>
-                <Button
-                  className="btn primary"
-                  onClick={() =>
-                    PanelStore.setActivePanel("/project-id", item.id)
-                  }
-                >
-                  Украсть чаевые
-                </Button>
-              </div>
-            </div>
-          );
-        })}
+    <div className="user">
+      <Badge count={topNumber}>
+        <a href={`https://vk.com/id${dataItem.id}`} target="_blank">
+          <Avatar size={40} shape="circle" src={dataItem.img} />
+        </a>
+      </Badge>
+
+      <div className="user-container">
+        <div className="info">
+          {dataItem.firstName} {dataItem.firstName}
+        </div>
+        <div className="info description">
+          {numberToString(dataItem.balance)}$
+        </div>
+        <Button
+          className="btn primary"
+          onClick={() => PanelStore.setActivePanel("/project-id", dataItem.id)}
+        >
+          Украсть чаевые
+        </Button>
       </div>
     </div>
   );
